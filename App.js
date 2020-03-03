efdf4e48947a42abe692f97757d01431584cf76f
Marya Doery <mdoery@gmail.com>
2020-03-03 17:15:38 -0500

    Add method componentDidMount to fetch data from an API which returns a list of movies. The console is used to log this information to the Metro server terminal.

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+	componentDidMount() {
+		return fetch('https://www.fullstackoasis.com/rnb/movies.php')
+			.then((response) => response.json())
+			.then((responseJson) => {
+				console.log(responseJson);
+				this.setState({
+					isLoading: false,
+					dataSource: responseJson.movies,
+				}, function () {
+				});
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+
 	render() {
 		return null;
 	}
