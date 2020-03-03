ea7392ef68590a911b3920578677ff8653712790
Marya Doery <mdoery@gmail.com>
2020-03-03 17:37:20 -0500

    A Picker has been added which displays the fetched list of movies.

--- App.js
+++ App.js
@@ -19,9 +19,21 @@ export default class App extends Component {
 	}
 
 	render() {
-		return <View>
-			<Picker>
-			</Picker>
-		</View>;
+		if (this.state && !this.state.isLoading) {
+			let items = [];
+			var length = this.state.dataSource.length;
+			for (var i = 0; i < length; i++) {
+				var item = this.state.dataSource[i];
+				items.push(<Picker.Item label={item.title}
+					value={item.title} key={item.id} />);
+			}
+			return <View>
+				<Picker>
+				{items}
+				</Picker>
+			</View>;
+		} else {
+			return (<View></View>);
+		}
 	}
 }
