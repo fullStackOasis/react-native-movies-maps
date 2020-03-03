91d21f4b177797bd0ff2589c0a21e56366e6fa84
Marya Doery <mdoery@gmail.com>
2020-03-03 16:56:00 -0500

    Replace the simple Hello World app with the demo "Ticking Clock" app like the React demo: https://reactjs.org/docs/state-and-lifecycle.html But we're doing this in React Native, so there are some small differences.

--- App.js
+++ App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { date: new Date() };
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		this.setState({
+			date: new Date()
+		});
+	}
+
+	render() {
+		return (
+			<View><Text>It is {this.state.date.toLocaleTimeString()}.</Text></View>
+		);
+	}
+}
+
 export default class App extends Component {
 	render() {
-		return (<View><Text>Hello, world!</Text></View>);
+		return (<Clock />);
 	}
 }
