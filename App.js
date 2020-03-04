dfb3dfe0b1faebdd88fe0fcff69e899947fa080f
Marya Doery <mdoery@gmail.com>
2020-03-03 20:51:02 -0500

    Add "waiting..." to the view when there's a network delay, to indicate that the app is at least functioning.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Picker, View } from 'react-native';
+import { Picker, View, Text } from 'react-native';
 
 export default class App extends Component {
 	componentDidMount() {
@@ -46,7 +46,7 @@ export default class App extends Component {
 				</Picker>
 			</View>;
 		} else {
-			return (<View></View>);
+			return (<View><Text>waiting...</Text></View>);
 		}
 	}
 }
