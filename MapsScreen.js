038f468c536cf608acb80552e5e1af973926b3cd
Marya Doery <mdoery@gmail.com>
2020-03-05 15:06:58 -0500

    Put a MapView on the page. But the page appears to be blank.

--- MapsScreen.js
+++ MapsScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import MapView from 'react-native-maps';
 export default class MapsScreen extends Component {
 	render() {
 		return (<View style={
@@ -11,7 +11,14 @@ export default class MapsScreen extends Component {
 				padding: 10
 			}]
 		}>
-			<Text>A map goes here</Text>
+			<MapView
+				initialRegion={{
+					latitude: 37.78825,
+					longitude: -122.4324,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421,
+				}}
+			/>
 		</View>);
 	}
 }
